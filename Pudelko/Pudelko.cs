5a3c1c8d533da55f8fe7a89f8bd0b527bcd7cf77
Mvtthew <mateusz2345678@gmail.com>
2020-11-26 10:39:02 +0100

    :+1: 13

--- Pudelko/Pudelko.cs
+++ Pudelko/Pudelko.cs
@@ -1,15 +1,16 @@
 ﻿
 using System;
+using System.Collections;
 
 namespace PudelkoLibrary
 {
 	class Pudelko : IFormattable
 	{
 		// private a,b,c always in mm for easier calculations
-		private double _A;
-		private double _B; 
-		private double _C; // in mm
-		private UnitOfMeasure unit;
+		private readonly  double _A;
+		private readonly double _B; 
+		private readonly double _C; // in mm
+		private readonly UnitOfMeasure unit;
 
 		// convert to m from private mm on the fly
 		public double A {
@@ -38,20 +39,39 @@ namespace PudelkoLibrary
 		}
 
 		private double ParseToMM(double value, UnitOfMeasure unit) {
-			switch(unit) {
+			switch (unit) {
 				case UnitOfMeasure.milimeter:
-					return value;
+					return Math.Truncate(value);
 				case UnitOfMeasure.centimeter:
-					return value * 10;
+					return value < 0.1 ? 0 : Math.Round(value * 10, 1);
 				case UnitOfMeasure.meter:
-					return value * 1000;
+					return value < 0.001 ? 0 : Math.Round(value * 1000, 3);
 				default: 
 					throw new ArgumentOutOfRangeException();
 			}
 		}
-
-		public Pudelko (double a = 0.1, double b = 0.1, double c = 0.1, UnitOfMeasure unit = UnitOfMeasure.meter) {
-			if (a <= 0 || b <= 0 || c <= 0) 
+		public Pudelko(double a, UnitOfMeasure unit = UnitOfMeasure.meter)
+		{
+			if (this.ParseToMM(a, unit) <= 0)
+				throw new ArgumentOutOfRangeException();
+			if (this.ParseToMM(a, unit) >= 10000)
+				throw new ArgumentOutOfRangeException();
+			this._A = this.ParseToMM(a, unit);
+			this._B = this.ParseToMM(0.1, UnitOfMeasure.meter);
+			this._C = this.ParseToMM(0.1, UnitOfMeasure.meter);
+		}
+		public Pudelko(double a, double b, UnitOfMeasure unit = UnitOfMeasure.meter)
+		{
+			if (this.ParseToMM(a, unit) <= 0 || this.ParseToMM(b, unit) <= 0)
+				throw new ArgumentOutOfRangeException();
+			if (this.ParseToMM(a, unit) > 10000 || this.ParseToMM(b, unit) > 10000)
+				throw new ArgumentOutOfRangeException();
+			this._A = this.ParseToMM(a, unit);
+			this._B = this.ParseToMM(b, unit);
+			this._C = this.ParseToMM(0.1, UnitOfMeasure.meter);
+		}
+		public Pudelko (double a, double b, double c, UnitOfMeasure unit = UnitOfMeasure.meter) {
+			if (this.ParseToMM(a, unit) <= 0 || this.ParseToMM(b, unit) <= 0 || this.ParseToMM(c, unit) <= 0) 
 				throw new ArgumentOutOfRangeException();
 			if (this.ParseToMM(a, unit) > 10000 || this.ParseToMM(b, unit) > 10000 || this.ParseToMM(c, unit) > 10000)
 				throw new ArgumentOutOfRangeException();
@@ -59,6 +79,7 @@ namespace PudelkoLibrary
 			this._B = this.ParseToMM(b, unit);
 			this._C = this.ParseToMM(c, unit);
 		}
+
 
 		public override string ToString()
 		{
@@ -132,9 +153,94 @@ namespace PudelkoLibrary
 			return obj.Equals(obj2);
 		}
 
+		public static Pudelko operator +(Pudelko obj, Pudelko obj2)
+		{
+			double a = obj._A + obj2._A;
+			double b = obj._B + obj2._B;
+			double c = obj._C > obj2._C ? obj._C : obj2._C;
+			return new Pudelko(a, b, c, UnitOfMeasure.milimeter);
+		}
+
+		public static explicit operator double[](Pudelko obj) {
+			var r = new double[3] { obj.A, obj.B, obj.C };
+			return r;
+		}
+
+		public static implicit operator Pudelko(ValueTuple<int, int, int> wymiary) {
+			return new Pudelko((double)wymiary.Item1, (double)wymiary.Item2, (double)wymiary.Item3, UnitOfMeasure.milimeter);
+		}
+
+		public double this[int index]
+		{
+			get {
+				switch (index) {
+					case 0:
+						return _A;
+					case 1:
+						return _B;
+					case 2:
+						return _C;
+					default:
+						throw new ArgumentOutOfRangeException();
+				}
+			}
+		}
+
+		int position = 0;
+		public bool MoveNext()
+		{
+			position++;
+			return (position < 3);
+		}
+		public void Reset()
+		{
+			position = 0;
+		}
+		public object Current
+		{
+			get {
+				switch(position) {
+					case 0:
+						return _A;
+					case 1:
+						return _B;
+					case 2:
+						return _C;
+					default:
+						return 0;
+				}
+			}
+		}
+
+		public IEnumerator GetEnumerator() {
+			return (IEnumerator)this;
+		}
+
+		public static Pudelko Parse(string toParse) {
+			var splitedToParse = toParse.Split(" ");
+			if (splitedToParse.Length == 8) {
+				var a = double.Parse(splitedToParse[0]);
+				var b = double.Parse(splitedToParse[3]);
+				var c = double.Parse(splitedToParse[6]);
+				var unitChar = splitedToParse[1];
+				switch(unitChar) {
+					case "mm":
+						return new Pudelko(a, b, c, UnitOfMeasure.milimeter);
+					case "cm":
+						return new Pudelko(a, b, c, UnitOfMeasure.centimeter);
+					case "m":
+						return new Pudelko(a, b, c, UnitOfMeasure.meter);
+					default:
+						throw new FormatException();
+				}
+			} else {
+				throw new FormatException();
+			}
+		}
+
 	}
 
-	interface IEquatable<Pudelko> {
-		bool Equals(Pudelko obj);
-	}
+	public interface IEquatable<Pudelko> { }
+
+	public interface IEnumerable<Pudelko> : System.Collections.IEnumerable { }
 }
